27ba2f499bb1fe7df9803ca205f6abac2364cb99
Enzo Quelenis <enzo.quelenis@saint-gobain.com>
2025-03-28 16:31:42 +0100

    fix 401 for manifest.json

--- src/main.tsx
+++ src/main.tsx
@@ -7,3 +7,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <App />
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
